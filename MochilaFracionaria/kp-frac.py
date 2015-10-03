ccce3cdbb1c4060ba0fe0ec26e82d49346645bf6
Gabriel Barros <gbarros@teradici.com>
2015-10-03 18:41:14 -0300

    SimpleSover
    To be used once there is only one try left

--- MochilaFracionaria/kp-frac.py
+++ MochilaFracionaria/kp-frac.py
@@ -1,3 +1,5 @@
+KEY = 0
+
 def mergesort(v):
 
 	if(len(v) == 1):
@@ -25,3 +27,17 @@ def mergesort(v):
 				break  
 
 	return res
+
+def throw(step):
+"""
+	Encapsulates the answer 
+"""
+	if step == KEY:
+		return True
+	else:
+		return False
+
+def simpleSolve(start = 0):
+	while throw(start) == False:
+		start += 1
+	return start
