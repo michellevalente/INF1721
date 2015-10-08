b1bb01214928e21077c9d20cbe2e4d9f30ddd8f8
Carlos Mattoso <calmattoso@hotmail.com>
2015-10-07 23:50:42 -0300

    Fix in range of Objects rearrangement loop.

--- MochilaFracionaria/kpfrac_linear_heuristic.cpp
+++ MochilaFracionaria/kpfrac_linear_heuristic.cpp
@@ -61,7 +61,7 @@ Object heuristic (Object * v, int n) {
 
     // Rearrange the list of objects.
     int store = 0;
-    for (int i = 0; i < n - 1; i++) {
+    for (int i = 0; i < n; i++) {
         if (v[i] < pivot) {
             std::swap(v[i], v[store++]);
         }
